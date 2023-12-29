d1edcfc99235d7d63d92e573de7b07c3889c5b2d
M V Midhun <midhunmareedu@gmail.com>
2023-12-29 19:37:19 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -36,4 +36,4 @@ async def main(file:UploadFile):
         return { "success" : "false" }
 
 if __name__ == "__main__" :
-    uvicorn.run("main:app" , host="127.0.0.1" , reload=True)
+    uvicorn.run("main:app" , host="0.0.0.0" , reload=True)
